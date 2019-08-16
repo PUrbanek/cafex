94fc75524cc3ea8f93ea3818ca95f5711d59fb27
PUrbanek <sheeganaw@gmail.com>
2019-08-16 12:03:06 +0200

    Step 2: Write tests, make them fail

--- src/test/scala/CafeXSpec.scala
+++ src/test/scala/CafeXSpec.scala
@@ -11,27 +11,36 @@ class CafeXSpec extends FlatSpec with Matchers {
   }
 
   it should "return 1.00 if Coffee is ordered" in {
-    CafeX.order(Seq("Coffee")) shouldEqual 1.00
-
+    CafeX.order(Seq("Coffee")) shouldEqual 1.20
   }
 
-  it should "return 2.00 if Cheese Sandwich is ordered" in {
-    CafeX.order(Seq("Cheese Sandwich")) shouldEqual 2.00
-
+  it should "return 2.20 if Cheese Sandwich is ordered" in {
+    CafeX.order(Seq("Cheese Sandwich")) shouldEqual 2.20
   }
 
-  it should "return 4.50 if Steak Sandwich is ordered" in {
-    CafeX.order(Seq("Steak Sandwich")) shouldEqual 4.50
-
+  it should "return 5.40 if Steak Sandwich is ordered" in {
+    CafeX.order(Seq("Steak Sandwich")) shouldEqual 5.40
   }
 
   it should "not count nonexistent items" in {
-    CafeX.order(Seq("Steak Sandwich", "Random")) shouldEqual 4.50
-
+    CafeX.order(Seq("Steak Sandwich", "Random")) shouldEqual 5.40
   }
 
   it should "add up many items correctly" in {
-    CafeX.order(Seq("Steak Sandwich", "Cola")) shouldEqual 5.00
+    CafeX.order(Seq("Steak Sandwich", "Cola")) shouldEqual 6.00
+  }
 
+  it should "not add more than £20 of service charge" in {
+    CafeX.order(Seq(
+      "Steak Sandwich", "Steak Sandwich", "Steak Sandwich",
+      "Steak Sandwich", "Steak Sandwich", "Steak Sandwich",
+      "Steak Sandwich", "Steak Sandwich", "Steak Sandwich",
+      "Steak Sandwich", "Steak Sandwich", "Steak Sandwich",
+      "Steak Sandwich", "Steak Sandwich", "Steak Sandwich",
+      "Steak Sandwich", "Steak Sandwich", "Steak Sandwich",
+      "Steak Sandwich", "Steak Sandwich", "Steak Sandwich",
+      "Steak Sandwich", "Steak Sandwich", "Steak Sandwich",
+      "Steak Sandwich", "Steak Sandwich", "Steak Sandwich"
+    )) shouldEqual 141.5
   }
 }
