904ee1f5e9e60f4bc543fe8a8fcdf70a069b2161
PUrbanek <sheeganaw@gmail.com>
2019-08-16 12:50:53 +0200

    Refactor to BigDecimal

--- src/main/scala/CafeX.scala
+++ src/main/scala/CafeX.scala
@@ -1,13 +1,13 @@
 object CafeX {
 
   private val items = Map(
-    "Cola" -> Item("Cola", 0.50, Seq(Drink, Cold)),
-    "Coffee" -> Item("Coffee", 1.00, Seq(Drink, Hot)),
-    "Cheese Sandwich" -> Item("Cheese Sandwich", 2.0, Seq(Food, Cold)),
-    "Steak Sandwich" -> Item("Steak Sandwich", 4.50, Seq(Food, Hot))
+    "Cola" -> Item("Cola", BigDecimal(0.50), Seq(Drink, Cold)),
+    "Coffee" -> Item("Coffee", BigDecimal(1.00), Seq(Drink, Hot)),
+    "Cheese Sandwich" -> Item("Cheese Sandwich", BigDecimal(2.0), Seq(Food, Cold)),
+    "Steak Sandwich" -> Item("Steak Sandwich", BigDecimal(4.50), Seq(Food, Hot))
   )
 
-  def calculatePrice(itemsOrdered: Seq[String]): Double = {
+  def calculatePrice(itemsOrdered: Seq[String]): BigDecimal = {
     val itemsMapped = itemsOrdered.filter(singleItem => items.contains(singleItem))
         .map(singleItem => items(singleItem))
     val sum = itemsMapped.map(_.price).sum
@@ -15,16 +15,16 @@ object CafeX {
     sum + serviceCharge
   }
 
-  private def determineServiceChargePercent(itemsOrdered: Seq[Item]): Double = {
+  private def determineServiceChargePercent(itemsOrdered: Seq[Item]): BigDecimal = {
     if (itemsOrdered.filter(_.categories.contains(Food)).exists(_.categories.contains(Hot))) 0.20
     else if (itemsOrdered.exists(_.categories.contains(Food))) 0.10
-    else 0
+    else 0.00
   }
 
-  private def calculateServiceCharge(sum: Double, serviceChargePercent: Double): Double = {
-    BigDecimal(sum * serviceChargePercent).setScale(2, BigDecimal.RoundingMode.HALF_UP) match {
-      case sc if sc > 20 => 20
-      case sc => sc.toDouble
+  private def calculateServiceCharge(sum: BigDecimal, serviceChargePercent: BigDecimal): BigDecimal = {
+    (sum * serviceChargePercent).setScale(2, BigDecimal.RoundingMode.HALF_UP) match {
+      case sc if sc > 20.00 => 20.00
+      case sc => sc
     }
   }
 }
--- src/main/scala/Item.scala
+++ src/main/scala/Item.scala
@@ -8,4 +8,4 @@ sealed trait TypeCategory extends Category
 case object Food extends TypeCategory
 case object Drink extends TypeCategory
 
-case class Item(name: String, price: Double, categories: Seq[Category])
+case class Item(name: String, price: BigDecimal, categories: Seq[Category])
